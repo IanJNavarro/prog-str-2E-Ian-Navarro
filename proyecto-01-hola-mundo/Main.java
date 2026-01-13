a253f4d97c9e8abf0052ad6ffae237968cf8ed2b
IanNavarro <inavar10@calpoly.edu>
2026-01-13 07:39:29 -0600

    Readme n hello world

---
+++ proyecto-01-hola-mundo/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Ian Josue Navarro Arce");
+        System.out.println("Grupo: E");
+        System.out.println("Fecha: 9/Ene/2026");
+        System.out.println("Hello World");
+    }
+}
