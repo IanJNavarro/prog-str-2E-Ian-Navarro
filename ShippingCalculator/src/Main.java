a86c6fb656b56c071a3a39d290930dd35529e036
IanNavarro <inavar10@calpoly.edu>
2026-02-04 19:12:01 -0600

    output added

--- ShippingCalculator/src/Main.java
+++ ShippingCalculator/src/Main.java
@@ -4,6 +4,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        System.out.println("El siguiente programa calcula el costo de envio de un producto en base a la distancia, peso y otros factores.");
+        
+
         Lectura.leerBoolean(sc,"Es una zona rural? (escribe solamente si o no)");
 
 
