e75a3dd208b0c9241fb64a4924c60cd625078178
IanNavarro <inavar10@calpoly.edu>
2026-03-12 09:32:47 -0600

    demolistview part 2

---
+++ DemoListViewPart2/src/main/java/com/example/demolistview/controllers/AppControllers.java
@@ -0,0 +1,78 @@
+package com.example.demolistview.controllers;
+
+import com.example.demolistview.services.PersonService;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+import java.util.List;
+
+public class AppControllers {
+    @FXML
+    private Label lblMsg;
+    @FXML
+    private ListView<String> listView;
+    @FXML
+    private TextField txtName;
+    @FXML
+    private TextField txtEmail;
+    @FXML
+    private TextField txtAge;
+
+    @FXML
+    private ObservableList<String> data = FXCollections.observableArrayList();
+
+    private PersonService service = new PersonService();
+
+    @FXML
+    public void initialize(){
+        loadFromFile();
+        listView.setItems(data);
+    }
+
+    @FXML
+    public void onReload(){
+        loadFromFile();
+    }
+
+    @FXML
+    public void onAdd(){
+        String name = txtName.getText();
+        String email = txtEmail.getText();
+        int age = 0;
+
+        try {
+            age = Integer.parseInt(txtAge.getText());
+            service.addPerson(name,email,age);
+            lblMsg.setStyle("-fx-text-fill: green");
+            lblMsg.setText("Persona creada con exito.");
+            txtEmail.clear();
+            txtName.clear();
+            txtAge.clear();
+        }catch (IOException IOException){
+            lblMsg.setStyle("-fx-text-fill: red");
+            lblMsg.setText("Error con el archivo.");
+        }catch (IllegalArgumentException argumentException){
+            lblMsg.setStyle("-fx-text-fill: red");
+            lblMsg.setText("Error con los datos.");
+        }
+
+    }
+
+    private void loadFromFile(){
+        try{
+            List<String> items = service.loadDataForListView();
+            data.setAll(items);
+            lblMsg.setStyle("-fx-text-fill: green");
+            lblMsg.setText("Datos cargados con exito");
+        }catch (IOException e){
+            lblMsg.setStyle("-fx-text-fill: red");
+            lblMsg.setText("Error al cargar los archivos.");
+        }
+
+    }
+}
---
+++ DemoListViewPart2/src/main/java/com/example/demolistview/services/PersonService.java
@@ -0,0 +1,49 @@
+package com.example.demolistview.services;
+
+import com.example.demolistview.repositories.PersonFilesRepository;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PersonService {
+
+    PersonFilesRepository repo = new PersonFilesRepository();
+
+    public List<String> loadDataForListView() throws IOException {
+
+        List<String> lines = repo.readAllLines();
+        List<String> result = new ArrayList<>();
+
+        for (String line: lines){
+            if (line==null || line.isBlank()) continue;
+
+            String[] parts = line.split(",");
+            String name = parts[0];
+            String email = parts[1];
+            String age = parts[2];
+
+            result.add("Name: "+name + " - Email: " + email + " - Age: " + age );
+        }
+
+        return result;
+    }
+
+    public void addPerson(String name, String email, int age) throws IOException {
+        validate(name, email, age);
+        repo.appendNewLine(name+","+email+","+age);
+    }
+
+    private void validate(String name, String email, int age){
+        if (name == null || name.isBlank() || name.length()<3){
+            throw new IllegalArgumentException("El nombre es incorrecto");
+        }
+        String emailConverted = (email==null) ? "" : email.trim();
+        if (emailConverted.isBlank() || !emailConverted.contains("@") || !emailConverted.contains(".")){
+            throw new IllegalArgumentException("El email es incorrecto");
+        }
+        if (age < 18 || age > 110 ){
+            throw new IllegalArgumentException("La edad no esta en un rango valido.");
+        }
+    }
+}
